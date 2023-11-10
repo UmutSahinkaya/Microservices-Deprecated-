fffb75383d99073cb6b0f552cf05c1c572886483
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-10 15:48:00 +0300

    Configuration and DI

--- Services/Basket/Services.Basket/Startup.cs
+++ Services/Basket/Services.Basket/Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Services.Basket.Services;
 using Services.Basket.Settings;
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,15 @@ namespace Services.Basket
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
+            services.AddSingleton<RedisService>(sp =>
+            {
+                var redisSettings=sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+
+                var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+                redis.Connect();
+                return redis;
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
