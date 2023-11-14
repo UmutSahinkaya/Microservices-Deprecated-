623b2a0e2c34200879e6dc5325c2fc1d8daa2348
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-14 11:01:22 +0300

    Dtos instances created in Application

---
+++ Services/Order/Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Order.Application.Dtos
+{
+    public class OrderItemDto
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+    }
+}
