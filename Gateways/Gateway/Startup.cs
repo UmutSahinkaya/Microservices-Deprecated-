cac0b40c8466168ddaa29277718aae18183472dd
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-15 14:40:02 +0300

    Gateway protected

--- Gateways/Gateway/Startup.cs
+++ Gateways/Gateway/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
@@ -14,10 +15,23 @@ namespace Gateway
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", opt =>
+            {
+                opt.Authority = _configuration["IdentityServerURL"];
+                opt.Audience = "resource_gateway";
+                opt.RequireHttpsMetadata = false;
+            });
             services.AddOcelot();
         }
 
