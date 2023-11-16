957d77d193316278e45148b8d449909b387725ba
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-16 16:12:16 +0300

    SigninInput Httppost action

--- FrontEnds/Web/Models/SigninInput.cs
+++ FrontEnds/Web/Models/SigninInput.cs
@@ -5,8 +5,10 @@ namespace Web.Models
     public class SigninInput
     {
         [Display(Name ="Email adresiniz")]
+        [Required]
         public string Email { get; set; }
         [Display(Name = "Şifreniz")]
+        [Required]
         public string Password { get; set; }
         [Display(Name = "Beni Hatırla")]
         public bool IsRemember { get; set; }
