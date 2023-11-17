9800d9966cb4bac9fdb9940cdd50bbf141891cbe
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-17 17:48:15 +0300

    CatalogService methods

--- FrontEnds/Web/Services/CatalogService.cs
+++ FrontEnds/Web/Services/CatalogService.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Shared.Dtos;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Web.Models.Catalog;
 using Web.Services.Interfaces;
@@ -15,39 +17,62 @@ namespace Web.Services
             _httpClient = httpClient;
         }
 
-        public Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        public async Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
         {
-            throw new System.NotImplementedException();
+            var response = await _httpClient.PostAsJsonAsync<CourseCreateInput>("courses", courseCreateInput);
+            return response.IsSuccessStatusCode;
         }
 
-        public Task<bool> DeleteCourseAsync(string courseId)
+        public async Task<bool> DeleteCourseAsync(string courseId)
         {
-            throw new System.NotImplementedException();
+            var response = await _httpClient.DeleteAsync($"courses/{courseId}");
+            return response.IsSuccessStatusCode;
         }
 
-        public Task<List<CategoryViewModel>> GetAllCategoryAsync()
+        public async Task<List<CategoryViewModel>> GetAllCategoryAsync()
         {
-            throw new System.NotImplementedException();
+            //http://localhost:5000/services/catalog/courses
+            var response = await _httpClient.GetAsync("categories");
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var responseSuccess = await response.Content.ReadFromJsonAsync<Response<List<CategoryViewModel>>>();
+            return responseSuccess.Data;
         }
 
-        public Task<List<CourseViewModel>> GetAllCourseAsync()
+        public async Task<List<CourseViewModel>> GetAllCourseAsync()
         {
-            throw new System.NotImplementedException();
+            //http://localhost:5000/services/catalog/courses
+            var response = await _httpClient.GetAsync("courses");
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var responseSuccess=await response.Content.ReadFromJsonAsync<Response<List<CourseViewModel>>>();
+            return responseSuccess.Data;
         }
 
-        public Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        public async Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
         {
-            throw new System.NotImplementedException();
+            //http://localhost:5000/services/catalog/courses/getallbyuserId/userId
+            var response = await _httpClient.GetAsync($"course/GetAllByUserId/{userId}");
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var responseSuccess = await response.Content.ReadFromJsonAsync<Response<List<CourseViewModel>>>();
+            return responseSuccess.Data;
         }
 
-        public Task<CourseViewModel> GetByCourseIdAsync(string courseId)
+        public async Task<CourseViewModel> GetByCourseIdAsync(string courseId)
         {
-            throw new System.NotImplementedException();
+            //http://localhost:5000/services/catalog/courses/userId
+            var response = await _httpClient.GetAsync($"course/{courseId}");
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var responseSuccess = await response.Content.ReadFromJsonAsync<Response<CourseViewModel>>();
+            return responseSuccess.Data;
         }
 
-        public Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        public async Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
         {
-            throw new System.NotImplementedException();
+            var response = await _httpClient.PutAsJsonAsync<CourseUpdateInput>("courses", courseUpdateInput);
+            return response.IsSuccessStatusCode;
         }
     }
 }
