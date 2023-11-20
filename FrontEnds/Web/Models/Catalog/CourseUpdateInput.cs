62531ecbd037039442427517279f155a5e4023d9
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-20 13:53:02 +0300

    CourseController/Update(for Cource photo update)

--- FrontEnds/Web/Models/Catalog/CourseUpdateInput.cs
+++ FrontEnds/Web/Models/Catalog/CourseUpdateInput.cs
@@ -1,14 +1,34 @@
-﻿namespace Web.Models.Catalog
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.Catalog
 {
     public class CourseUpdateInput
     {
         public string Id { get; set; }
+        [Display(Name = "Kurs ismi")]
+        [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Kurs açıklama")]
+        [Required]
         public string Description { get; set; }
+
+        [Display(Name = "Kurs fiyat")]
+        [Required]
         public decimal Price { get; set; }
+
         public string Picture { get; set; }
+
         public string UserId { get; set; }
+
         public FeatureViewModel Feature { get; set; }
+
+        [Display(Name = "Kurs kategori")]
+        [Required]
         public string CategoryId { get; set; }
+
+        [Display(Name = "Kurs Resmi")]
+        public IFormFile PhotoFormFile { get; set; }
     }
 }
