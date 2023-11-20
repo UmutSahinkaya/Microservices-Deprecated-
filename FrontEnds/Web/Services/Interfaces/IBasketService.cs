271902afe41b0d8a600c2891414ff11cd06cde9f
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-11-20 16:00:43 +0300

    IBasketService/BasketService

---
+++ FrontEnds/Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Web.Models.Basket;
+
+namespace Web.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+
+        Task<BasketViewModel> Get();
+
+        Task<bool> Delete();
+
+        Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+
+        Task<bool> RemoveBasketItem(string courseId);
+
+        Task<bool> ApplyDiscount(string discountCode);
+
+        Task<bool> CancelApplyDiscount();
+    }
+}
